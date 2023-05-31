e2213830dc6e996d5d50ab97879d2858fc9dc956
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-05-31 22:50:44 +0300

    feat: add google fonts to app file

--- nextjs-blog/src/pages/_app.tsx
+++ nextjs-blog/src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter, Roboto } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+const roboto = Roboto({
+	weight: ["400", "700"],
+	style: ["normal", "italic"],
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-roboto",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<div className={`${roboto.variable} ${inter.variable}`}>
+			<Component {...pageProps} />
+		</div>
+	);
 }
