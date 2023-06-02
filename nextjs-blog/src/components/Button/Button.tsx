833572b79d577461ed7f301bb1934b8d3ef468a3
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-06-02 09:07:40 +0300

    refactor: update component button

--- nextjs-blog/src/components/Button/Button.tsx
+++ nextjs-blog/src/components/Button/Button.tsx
@@ -1,14 +1,22 @@
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const Button = () => {
 	const { systemTheme, theme, setTheme } = useTheme();
 	const currentTheme = theme === "system" ? systemTheme : theme;
+	const [classes, setClasses] = useState(currentTheme);
+
+	useEffect(() => {
+		if (classes) setTheme(classes);
+	}, [classes, setTheme]);
 
 	return (
 		<button
-			onClick={() => (theme === "dark" ? setTheme("light") : setTheme("dark"))}
-			className='w-6 p-0.5 inline-block bg-gray-100 dark:bg-white-200 rounded-full'
+			onClick={() =>
+				theme === "dark" ? setClasses("light") : setClasses("dark")
+			}
+			className='min-w-6 p-0.5 inline-block bg-gray-100 dark:bg-white-200 rounded-full'
 		>
 			{theme === "dark" ? (
 				<Image
